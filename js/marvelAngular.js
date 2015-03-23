2a09b4302912cf138f86f0f77636cd5e9ce5663a
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-03-23 11:49:04 -0500

    second commit

--- js/marvelAngular.js
+++ js/marvelAngular.js
@@ -1,12 +1,20 @@
 var app = angular.module('app', []);
 app.controller ('marvelContrl', function($scope){
-var heroName='hulk';
-$scope.wooCharacter = heroName;
+var heroName='';
 $scope.woo = function(){
+	$scope.wooCharacter = heroName;
+	$('.name').html('');
+	$('.description').html('');
+	$('.image').html('');
+	$('.events').html('');
 	$.getJSON("http://gateway.marvel.com:80/v1/public/characters?name=" + heroName + "&apikey=9b468921eeceda45d379088e81c48169", function (json) {
   $(".name").append(json.data.results[0].name);
   $(".description").append(json.data.results[0].description);
   $(".image").attr("src", json.data.results[0].thumbnail.path + "/detail.jpg");
+  $.each(json.data.results[0].stories.items, function(i, item){
+  		$('.events').append("<li>"+item.name+"</li>");
+
+  })
    });
         };
 
