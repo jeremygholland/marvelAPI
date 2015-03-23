dfe34b802050a3f2069e169a565c58c5715f710f
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-03-23 12:39:53 -0500

    4th commit

--- js/marvelAngular.js
+++ js/marvelAngular.js
@@ -7,13 +7,18 @@ $scope.woo = function(){
 	$('.description').html('');
 	$('.image').html('');
 	$('.events').html('');
-	$.getJSON("http://gateway.marvel.com:80/v1/public/characters?name=" + heroName + "&apikey=9b468921eeceda45d379088e81c48169", function (json) {
+	$.getJSON("http://gateway.marvel.com:80/v1/public/characters?name=" + heroName + "&limit=10&apikey=9b468921eeceda45d379088e81c48169", function (json) {
   $(".name").append(json.data.results[0].name);
   $(".description").append(json.data.results[0].description);
   $(".image").attr("src", json.data.results[0].thumbnail.path + "/detail.jpg");
-  $.each(json.data.results[0].series.items, function(i, item){
+  $.each(json.data.results[0].events.items, function(i, item){
+  		if (json.data.results[0].events.items.length >10){
+  			json.data.results[0].events.items.length = 10;
+  			$('.events').append("<li>"+item.name+"</li>");
+  		}
+  		else{
   		$('.events').append("<li>"+item.name+"</li>");
-
+  	}
   })
    });
         };
