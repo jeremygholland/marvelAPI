3a70d0dfae02db071f6eac40ae59e57d2ba09c9c
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-03-23 22:39:46 -0500

    9th commit

--- js/marvelAngular.js
+++ js/marvelAngular.js
@@ -8,66 +8,75 @@ app.controller ('marvelContrl', function($scope){
 
 $scope.woo = function(){
 	$('.events').html("<li> </li>");
-	firstCall();
-	secondCall();
-	}
+	async.series({
+	one: function(callback){
+		heroName =$scope.wooCharacter;
+			$('.name').html('');
+			$('.description').html('');
+			$('.image').html('');
+			$('.events').html('');
+			$.getJSON("http://gateway.marvel.com:80/v1/public/characters?name=" + heroName + "&limit=100&apikey=9b468921eeceda45d379088e81c48169").then(function (json) {
+  		$(".name").append(json.data.results[0].name);
+  		$(".description").append(json.data.results[0].description);
+  		$(".image").attr("src", json.data.results[0].thumbnail.path + "/detail.jpg");
+  			$.each(json.data.results[0].events.items, function(i, item){ 
+  				marvel1.push(item.name);
 
-	var firstCall = function(){
-	heroName =$scope.wooCharacter;
-	$('.name').html('');
-	$('.description').html('');
-	$('.image').html('');
-	$('.events').html('');
-	$.getJSON("http://gateway.marvel.com:80/v1/public/characters?name=" + heroName + "&limit=100&apikey=9b468921eeceda45d379088e81c48169").then(function (json) {
-		var charOneID = json.data.results[0].id; 
-		console.log(charOneID);
-  $(".name").append(json.data.results[0].name);
-  $(".description").append(json.data.results[0].description);
-  $(".image").attr("src", json.data.results[0].thumbnail.path + "/detail.jpg");
-  	$.each(json.data.results[0].events.items, function(i, item){ 
-  		marvel1.push(item.name);	
-  });
+  		});
+  			console.log(marvel1);	
 
   
-  		});
-};
+  				});
+			setTimeout(function(){
+				callback(null, 1);
 
-var secondCall = function(){
-	$('.name1').html('');
-	$('.description1').html('');
-	$('.image1').html('');
-	heroName2 = $scope.wooCharacter2;
-	$.getJSON("http://gateway.marvel.com:80/v1/public/characters?name=" + heroName2 + "&limit=100&apikey=9b468921eeceda45d379088e81c48169").then( function (json) {
-		$(".name1").append(json.data.results[0].name);
-  		$(".description1").append(json.data.results[0].description);
-  		$(".image1").attr("src", json.data.results[0].thumbnail.path + "/detail.jpg");
-		$.each(json.data.results[0].events.items, function(i,item){
-			marvel2.push(item.name);	
-		});
-		alert(marvel2);
-	}).then(function(){
-		console.log("You have done " +marvel1);
-	alert("You have 2nd done" +marvel2);
-	var arr = marvel1.concat(marvel2);
+			}, 300);
+	},
+	two: function(callback){
+		$('.name1').html('');
+			$('.description1').html('');
+			$('.image1').html('');
+			heroName2 = $scope.wooCharacter2;
+			$.getJSON("http://gateway.marvel.com:80/v1/public/characters?name=" + heroName2 + "&limit=100&apikey=9b468921eeceda45d379088e81c48169").then( function (json) {
+				$(".name1").append(json.data.results[0].name);
+  				$(".description1").append(json.data.results[0].description);
+  				$(".image1").attr("src", json.data.results[0].thumbnail.path + "/detail.jpg");
+				$.each(json.data.results[0].events.items, function(i,item){
+					marvel2.push(item.name);	
+			});
 
-	var sorted_arr = arr.sort();
-	var results = [];
-	for (var i = 0; i<arr.length -1; i ++){
-		if(sorted_arr[i+1] == sorted_arr[i]){
-			results.push(sorted_arr[i]);
-		}
+			})
+			setTimeout(function(){
+				callback(null, 2);
+			}, 200);
+		},
+		three: function(callback){
+
+
+		var arr = marvel1.concat(marvel2);
+			var sorted_arr = arr.sort();
+			var results = [];
+			for (var i = 0; i<arr.length -1; i ++){
+				if(sorted_arr[i+1] == sorted_arr[i]){
+					results.push(sorted_arr[i]);
+				}
+			}
+			setTimeout(function(){
+				callback(null, 3);
+			}, 100);
+		$('.events').html("<li>"+ results + "</li>");
+		console.log(results);
 	}
-	alert(results);
-$('.events').html("<li>"+ results + "</li>");
-
-	})
-};
-
-
+		},
+		function(err, results){
+			console.log(results);
 });
 
+	}
 
 
+
+	});
 
 
 
