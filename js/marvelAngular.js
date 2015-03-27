a1ce6324913bb39db348d9e9f61108bf5d0eea7f
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-03-27 00:00:57 -0500

    03/27/2015 first commit

--- js/marvelAngular.js
+++ js/marvelAngular.js
@@ -1,11 +1,13 @@
 var app = angular.module('app', []);
 app.controller ('marvelContrl', function($scope){
+	$('.newDiv').hide();
 	var heroName;
 	var heroName2;
 	var seriesName;
 	var marvel1 = [];
 	var marvel2 = [];
 	var results1 = [];
+	var wooStuff;
 $scope.main = true;
 $scope.newDiv= false;
 
@@ -70,12 +72,28 @@ $scope.woo = function(){
 			
 			}
 				for (var j = 0; j<results1.length; j ++){
-						$('.events').append('<li id = '+j+'><a href= #> '+results1[j]+'</a></li>');
+						$('.events').append('<li id = '+j+'><a href= #>'+results1[j]+'</a></li>');
 						$('#'+j).click(function(){
+							var charList = [];
+							$('.main').hide();
+							$('.newDiv').show();
 
 							var tryIt = $(this).text();
 							console.log(tryIt);
-						});
+							$.getJSON('http://gateway.marvel.com:80/v1/public/events?name=' + tryIt + '&apikey=9b468921eeceda45d379088e81c48169').then (function (json){
+							$(".eventName").append(json.data.results[0].title);
+							$(".eventDesc").append(json.data.results[0].description);
+							var wooStuff = (json.data.results[0].id);
+							console.log(wooStuff);
+							$.getJSON('http://gateway.marvel.com:80/v1/public/events/'+wooStuff+'/characters?limit=100&apikey=9b468921eeceda45d379088e81c48169').then (function (json){
+								for(f = 0; f<json.data.results.length; f++ ){
+									var shit = json.data.results[f].name;
+								console.log(json.data.results.length);
+									$('.eventChar').append('<li>'+json.data.results[f].name+'</li>');
+								}
+							})
+							});
+					});
 					}
 
 			//for loop on results1
