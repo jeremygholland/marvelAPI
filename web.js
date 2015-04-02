e86da39942351d6059d4acf2471aab31854c5f1f
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-04-01 20:00:33 -0500

    breaking express with new routing

--- web.js
+++ web.js
@@ -1,8 +1,14 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
+var router = express.Router();
 var app = express();
 
-// app.use(morgan('dev'));
+app.get('/', "index.html");
+
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || port)
+
+// router.get('/', function(req, res) {
+//   res.send('Birds home page');
+// });
